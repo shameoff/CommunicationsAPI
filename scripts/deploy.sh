1a3e10bf40b7c2184079c680e2e371100c08717d
Egor Shamov <eshamov030316@gmail.com>
2023-06-17 22:39:06 +0700

    Фикс ошибки с переменной среды nginx.conf v5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,8 @@ export APP_PORT=9996
 export SERVER_NAME=api.shameoff.site
 
 # Настраиваем конфиг nginx.conf
-sed -i "s/\templateServerName/$SERVER_NAME/g" nginx.conf
-sed -i "s/\templateAppPort/$APP_PORT/g" nginx.conf
+sed -i "s/templateServerName/$SERVER_NAME/g" nginx.conf
+sed -i "s/templateAppPort/$APP_PORT/g" nginx.conf
 mv nginx.conf /etc/nginx/sites-available/communications_api
 ln -s /etc/nginx/sites-available/communications_api /etc/nginx/sites-enabled
 service nginx restart
