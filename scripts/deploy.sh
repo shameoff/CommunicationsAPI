2f810f0c50d351d58a205c11c764d836557c8701
Egor Shamov <eshamov030316@gmail.com>
2023-06-18 00:20:50 +0700

    Добавил проверку на то, запущен ли контейнер

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export APP_PORT=9996
+export APP_CONTAINER_NAME=communications_api_app
 export SERVER_NAME=api.shameoff.site
 export DEBUG=0
-
 # Настраиваем конфиг nginx.conf
 sed -i "s/templateServerName/$SERVER_NAME/g" nginx.conf
 sed -i "s/templateAppPort/$APP_PORT/g" nginx.conf
@@ -11,4 +11,8 @@ ln -s /etc/nginx/sites-available/communications_api /etc/nginx/sites-enabled
 service nginx restart
 
 # Запускаем контейнер
+if [ "$(docker ps -q -f name=$APP_CONTAINER_NAME)" ]; then
+    docker compose down
+    docker rm $APP_CONTAINER_NAME
+fi
 docker compose up -d --build
