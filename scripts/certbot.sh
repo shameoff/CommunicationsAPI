eef988b172bf7a03c3255cc4e176fbc0e2a7d9a9
Egor Shamov <eshamov030316@gmail.com>
2023-06-19 04:28:00 +0700

    Поменял sed на awk в скрипте для конфига сертификата TLS

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -18,32 +18,36 @@ has_certificate=$?
 if [ $has_certificate -eq 0 ]; then
     # Сертификат уже существует, проверяем, нужно ли его обновить
     certbot renew --dry-run
-    echo "Дебаг. Сертификат проверили"
 else
-    echo "Дебаг. Сертификат создаётся новый"
     # Сертификат не существует, создаем новый
     certbot certonly --webroot -w "/var/www/code/$APP_NAME" -d "$DOMAIN" --email "$EMAIL" --agree-tos
 fi
 
-echo "Дебаг.Дошли до переменной CERT_CONFIG"
-
 # Часть конфигурации, которая вставляется в конфиг сервера, чтобы автоматически конфигурировать сертификат
 CERT_CONFIG="listen 443 ssl; # managed by Certbot
-  ssl_certificate \/etc\/letsencrypt\/live\/$DOMAIN\/fullchain.pem; # managed by Certbot
-  ssl_certificate_key \/etc\/letsencrypt\/live\/$DOMAIN\/privkey.pem; # managed by Certbot
-  include \/etc\/letsencrypt\/options-ssl-nginx.conf; # managed by Certbot
-  ssl_dhparam \/etc\/letsencrypt\/ssl-dhparams.pem; # managed by Certbot
+  ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem; # managed by Certbot
+  ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem; # managed by Certbot
+  include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
 } server {
     if (\$host = $DOMAIN) {
-        return 301 https:\/\/\$host\$request_uri;
+        return 301 https://\$host\$request_uri;
     } # managed by Certbot
 
     listen 80;
     server_name $DOMAIN;
-    return 404; # managed by Certbot
-"
-sed -i "s/# INSERT_CERT_CONFIG_HERE/$CERT_CONFIG/" "/etc/nginx/sites-available/$APP_NAME"
-echo "Дебаг. Поменяли конфиг nginx.conf"
+    return 404; # managed by Certbot"
+
+awk -v cert_config="$CERT_CONFIG" -v app_name="$APP_NAME" '
+BEGIN {
+  RS = ""
+  ORS = "\n\n"
+}
+{
+  gsub("# INSERT_CERT_CONFIG_HERE", cert_config)
+  print
+}
+' "/etc/nginx/sites-available/$APP_NAME" temp_file && mv temp_file "/etc/nginx/sites-available/$APP_NAME"
 
 # Проверяем наличие cronjob для автоматического обновления
 if ! crontab -l | grep -q "$CRONJOB_NAME"; then
