f1776f8b10fa0562f6662446fe17b6decf89e009
Egor Shamov <eshamov030316@gmail.com>
2023-06-20 00:18:40 +0700

    Удалил случайно добавленную директорию postgres_data/

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ProjectConfig project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,19 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
 
-from events.views import EventAPIView
+from communications.views import CommunicationsViewSet, InterlocutorViewSet
+
+router = routers.DefaultRouter()
+router.register('interlocutors', InterlocutorViewSet, basename='interlocutors')
+router.register('communications', CommunicationsViewSet, basename="communications")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/events/', EventAPIView.as_view()),
-    path('api/v1/communications/', EventAPIView.as_view()),
+    path('api/v1/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import ExtendedUser
+
 # Register your models here.
+admin.site.register(ExtendedUser)
