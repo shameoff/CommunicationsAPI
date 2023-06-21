f26a0f564163ff0d62a770cd64a9c77d4d0b2bc6
Egor Shamov <eshamov030316@gmail.com>
2023-06-21 17:05:47 +0700

    Коммит для дебага времени работы пайплайна

--- scripts/nginx_certbot_cronjob.sh
+++ scripts/nginx_certbot_cronjob.sh
@@ -7,15 +7,18 @@ mv nginx.conf "/etc/nginx/sites-available/${APP_NAME}"
 ln -s "/etc/nginx/sites-available/${APP_NAME}" /etc/nginx/sites-enabled
 
 # Проверяем, установлен ли Certbot
+echo "Time before checking if installed $(date +%T)"
 if ! command -v certbot &> /dev/null; then
     echo "Certbot не установлен. Установите Certbot для продолжения."
     exit 1
 fi
+echo "Time after checking if installed $(date +%T)"
 
 # Проверяем наличие сертификата
 certbot certificates --domains "$APP_DOMAIN" | grep -q "Expiry Date"
 has_certificate=$?
 
+echo "Time before checking if cert exists $(date +%T)"
 if [ $has_certificate -eq 0 ]; then
     # Сертификат уже существует, проверяем, нужно ли его обновить
     certbot renew --dry-run
@@ -23,6 +26,7 @@ else
     # Сертификат не существует, создаем новый
     certbot certonly --webroot -w "/var/www/code/$APP_NAME" -d "$APP_DOMAIN" --email "$MY_EMAIL" --agree-tos
 fi
+echo "Time after checking if cert exists $(date +%T)"
 
 # Часть конфигурации, которая вставляется в конфиг сервера, чтобы автоматически конфигурировать сертификат
 CERT_CONFIG="listen 443 ssl; # managed by Certbot
@@ -49,6 +53,7 @@ BEGIN {
   print
 }
 ' "/etc/nginx/sites-available/$APP_NAME" > temp_file && mv temp_file "/etc/nginx/sites-available/$APP_NAME"
+echo "Time after awk replacing $(date +%T)"
 
 # Проверяем наличие cronjob для автоматического обновления
 if ! crontab -l | grep -q "certbot renew"; then
@@ -58,3 +63,4 @@ if ! crontab -l | grep -q "certbot renew"; then
 else
     echo "cronjob для автоматического обновления уже существует."
 fi
+echo "Time after crontab creating $(date +%T)"
