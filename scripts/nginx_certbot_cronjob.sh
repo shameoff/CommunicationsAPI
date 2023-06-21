e55466971d04fc2568a4c3a9b74ba478bf29d81b
Egor Shamov <eshamov030316@gmail.com>
2023-06-21 21:42:08 +0700

    Фикс nginx конфига

--- scripts/nginx_certbot_cronjob.sh
+++ scripts/nginx_certbot_cronjob.sh
@@ -28,18 +28,20 @@ fi
 
 # Часть конфигурации, которая вставляется в конфиг сервера, чтобы автоматически конфигурировать сертификат
 CERT_CONFIG="listen 443 ssl; # managed by Certbot
-  ssl_certificate /etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem; # managed by Certbot
-  ssl_certificate_key /etc/letsencrypt/live/$APP_DOMAIN/privkey.pem; # managed by Certbot
-  include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-} server {
+    ssl_certificate /etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/$APP_DOMAIN/privkey.pem; # managed by Certbot
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+}
+server {
     if (\$host = $APP_DOMAIN) {
         return 301 https://\$host\$request_uri;
     } # managed by Certbot
 
     listen 80;
     server_name $APP_DOMAIN;
-    return 404; # managed by Certbot"
+    return 404; # managed by Certbot
+"
 
 awk -v cert_config="$CERT_CONFIG" -v app_name="$APP_NAME" '
 BEGIN {
@@ -47,7 +49,7 @@ BEGIN {
   ORS = "\n\n"
 }
 {
-  gsub("# INSERT_CERT_CONFIG_HERE", cert_config)
+  gsub("listen 80;", cert_config)
   print
 }
 ' "/etc/nginx/sites-available/$APP_NAME" > temp_file && mv temp_file "/etc/nginx/sites-available/$APP_NAME"
