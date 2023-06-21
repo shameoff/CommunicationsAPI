21f9824fb78cec0335dc822211f621f6aa745d4a
Egor Shamov <eshamov030316@gmail.com>
2023-06-22 06:15:51 +0700

    Добавил подсчёт рейтинга

--- communications/serializers.py
+++ communications/serializers.py
@@ -15,24 +15,43 @@ class CommunicationsSerializer(ModelSerializer):
 
 
 class InterlocutorListSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField()
+
     class Meta:
         model = Interlocutor
-        fields = ('id', 'name', 'description')
+        fields = ('id', 'name', 'description', 'rate', 'avatar_url')
+
+    def get_avatar_url(self, obj):
+        pass
+
+    def get_rate(self, obj):
+        communications = obj.communication_set.all()
+        total_rate = sum(communication.rate for communication in communications)
+        return total_rate
 
 
 class InterlocutorDetailSerializer(ModelSerializer):
     communications = serializers.SerializerMethodField()
+    rate = serializers.SerializerMethodField()
 
     class Meta:
         model = Interlocutor
-        fields = ["id", "name", "description", "communications", "owner"]
-        read_only_fields = ["id", "communications", "owner"]
+        fields = ["id", "name", "description", "communications", "owner", 'rate', 'avatar_url', 'avatar_id']
+        read_only_fields = ["id", "communications", "owner", 'rate']
+
+    def get_avatar_url(self, obj):
+        pass
 
     def get_communications(self, obj):
         communications = obj.communication_set.all()
         # Сериализуем диалоги
         serializer = CommunicationsSerializer(communications, many=True)
         return serializer.data
+
+    def get_rate(self, obj):
+        communications = obj.communication_set.all()
+        total_rate = sum(communication.rate for communication in communications)
+        return total_rate
 
     def create(self, validated_data):
         validated_data['owner'] = self.context['request'].user
